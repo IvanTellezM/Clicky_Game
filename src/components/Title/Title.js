6dc0140dcdc28f853c774f190cbba5ed68ae550e
Ivan Tellez <ivantellezm@gmail.com>
2018-06-26 03:43:18 -0700

    updated all files, app works

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,12 +3,17 @@ import "./Title.css";
 
 const Title = props =>  (
 
-<div className='titleBar'>
-<h1 className="title">{props.title}</h1>
-<h3 className='instructions'>{props.instructions}</h3>
-<h3 className='score'>Score: {props.score}</h3>
-<h3 className='highscore'>Highscore: {props.highscore}</h3>
-</div>
+<div class="jumbotron jumbotron-fluid">
+  
+  <div className='titleBar'>
+     <h1 className="title">{props.title}</h1>
+     <h3 className='instructions'>{props.instructions}</h3>
+     <h3 className='score'>Score: {props.score}</h3>
+     <h3 className='highscore'>Highscore: {props.highscore}</h3>
+   </div>
+  </div>
+
+
 )
 
 export default Title;
